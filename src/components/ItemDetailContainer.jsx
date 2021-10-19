33c6eb9358061743d8caeeb3c0bb3dfe1ff9c103
Carlos Ruiz <caosrupe@gmail.com>
2021-10-18 21:57:07 -0300

    correccion de codigo

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,26 +2,27 @@ import ItemDetail from './ItemDetail';
 import React, {useState,useEffect} from 'react';
 
 
-
 const ItemDetailContainer = () => {
 
-    const[productos,setProductos]=useState()
+    const[producto,setProducto]=useState(null)
     useEffect(()=>{
 
     setTimeout(()=>{
             fetch("https://api.mercadolibre.com/sites/MLA/search?q=parlantepionner=2")
 .then(response=>response.json())
-.then(data=>setProductos(data.results));
+.then(data=>setProducto(data.results[0]));
         },5)
 
     },[]);
 
     return (
     <div className="Detalle">
-        {
-          productos &&  productos.map(item=>
-            <ItemDetail key={item.id} jsonpack={item} />
-        )}
+        {producto?
+          <ItemDetail jsonpack={producto} />
+        :
+            <h4>Cargando</h4>
+
+        }
 
 
     </div>
